065c3aa7a84e4b274f8ad333d1d912ae979d838b
ebanh <ebanhernandez@gmail.com>
2022-12-03 01:45:06 +0900

    implemented part two of day 1 challenge

--- challenge1/main.py
+++ challenge1/main.py
@@ -1,19 +1,28 @@
 
-def most_cal(file):
+def file_to_list(file):
     with open(file) as f:
         lines = f.read()
 
     elves = [[int(y) for y in x.split('\n')] for x in lines.split("\n\n")]
-    largest = 0
+    top_three(elves)
 
+
+def most_cal(elves):
     for elf in elves:
         cal = sum(elf)
         if cal > largest:
             largest = cal
-
     print(largest)
 
+def top_three(elves):
+    sum_list = []
+    for elf in elves:
+        sum_list.append(sum(elf))
+    sum_list.sort(reverse=True)
+    top_three = sum_list[0:3]
+    print(sum(top_three))
+
 def main():
-    most_cal('input.txt')
+    file_to_list('input.txt')
 
 main()
