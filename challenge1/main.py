504f881524de4b8e257169a055844faaa925020a
ebanh <ebanhernandez@gmail.com>
2022-12-03 01:48:53 +0900

    modify to print out both answers

--- challenge1/main.py
+++ challenge1/main.py
@@ -4,15 +4,17 @@ def file_to_list(file):
         lines = f.read()
 
     elves = [[int(y) for y in x.split('\n')] for x in lines.split("\n\n")]
+    most_cal(elves)
     top_three(elves)
 
 
 def most_cal(elves):
+    largest = 0
     for elf in elves:
         cal = sum(elf)
         if cal > largest:
             largest = cal
-    print(largest)
+    print(f"Largest amount of Calories: {largest}")
 
 def top_three(elves):
     sum_list = []
@@ -20,7 +22,7 @@ def top_three(elves):
         sum_list.append(sum(elf))
     sum_list.sort(reverse=True)
     top_three = sum_list[0:3]
-    print(sum(top_three))
+    print(f"Sum of top 3 largest calories: {sum(top_three)}")
 
 def main():
     file_to_list('input.txt')
